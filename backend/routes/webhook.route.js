590f49358ffa905b2ac7e42a06070bab8c13dc58
Samyak-Waghmare <samyakwaghmare347@gmail.com>
2026-06-18 20:04:14 +0530

    add api routes for webhook.route

---
+++ backend/routes/webhook.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { stripeWebhook } from "../controllers/webhook.controller.js";
+
+const router = express.Router();
+
+router.post("/stripe", express.raw({ type: 'application/json' }), stripeWebhook);
+
+export default router;
