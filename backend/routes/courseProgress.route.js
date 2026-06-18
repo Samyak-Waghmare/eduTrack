eaf271432205ef20abaef239b0304802c518cdaa
Samyak-Waghmare <samyakwaghmare347@gmail.com>
2026-06-18 20:04:13 +0530

    add api routes for courseProgress.route

---
+++ backend/routes/courseProgress.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+import isAuthenticated from "../middleware/isAuthenticated.js";
+import {
+    getCourseProgress,
+    updateLectureProgress,
+    markAsCompleted,
+    markAsInCompleted
+} from "../controllers/courseProgress.controller.js";
+
+const router = express.Router();
+
+router.route("/:courseId").get(isAuthenticated, getCourseProgress);
+router.route("/:courseId/lecture/:lectureId/view").post(isAuthenticated, updateLectureProgress);
+router.route("/:courseId/complete").post(isAuthenticated, markAsCompleted);
+router.route("/:courseId/incomplete").post(isAuthenticated, markAsInCompleted);
+
+export default router;
